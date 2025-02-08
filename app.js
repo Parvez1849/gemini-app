edcc5b2c65a7315f6e56e4b97360a8196e7c61b1
Parvez Khan <Parvezkhan10220@gmail.com>
2025-02-08 14:37:46 +0530

    demo

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+app.use(bodyParser.json());
+
+app.post('/getResponse', (req, res) => {
+    console.log(req.body.question)
+    const genAI = new GoogleGenerativeAI("AIzaSyDwwa1rGiCy28t3xggm4F0NH1Bqm0qoGOc");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Explain how AI works";
+
+model.generateContent(req.body.question).then(result=>{
+    console.log(result.response.text());
+    const response = result.response.text();
+    res.status(200).json({
+        response:response
+    })
+})
+.catch(err=>{
+    console.log(err);
+    res.status(500).json({
+        error:err
+    })
+})
+})
+
+app.use('*',()=>{
+    res.status(404).json({
+        msg:'bad request'
+    })
+})
+
+
+module.exports = app;
